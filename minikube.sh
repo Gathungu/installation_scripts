b9e462c2706c8b3ca95635f6311b9cb2190e4b0a
Gathungu <78884579+Gathungu@users.noreply.github.com>
2024-08-16 00:47:26 -0500

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 
+# Make sure you have kubectl installed. If not, you can install it via the terminal:
 
+sudo apt-get update && sudo apt-get install -y kubectl
+
+#Ensure that curl is also installed:
+
+
+
+# Installing Minikube:
+# Step 1: Install VirtualBox (if not already installed)
+
+sudo apt-get update
+sudo apt-get install -y virtualbox virtualbox-ext-pack
+
+# Step 2: Install Minikube
+# Download the latest version of Minikube using curl:
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+&& chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+# Step 3: Start Minikubesudo apt-get update && sudo apt-get install -y curl
+# Begin Minikube with the following command, setting VirtualBox as the VM driver:
+
+minikube start - vm-driver=virtualbox
+
+# 2. Confirm the start & status of your Minikube cluster:
+
+minikube start 
+minikube status
+
+
+##############################################################################################################################
+OLD CODE
+##############################################################################################################################
 sudo apt-get update -y
 
 sudo apt-get install curl wget apt-transport-https virtualbox virtualbox-ext-pack -y
@@ -34,5 +66,10 @@ kubectl version -o yaml
 echo "Start the minikube"
 minikube start 
 minikube status
+
+
+
+
+
 
 
